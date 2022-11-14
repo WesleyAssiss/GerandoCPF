0384aae373d4f2489d825627b392774c0fed19c5
Wesley_Assis <88063740+WesleyAssis1999@users.noreply.github.com>
2022-11-14 04:43:56 -0300

    Update Cpf.java

--- src/GerandoCPF/Cpf.java
+++ src/GerandoCPF/Cpf.java
@@ -1,8 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Wesley Elbert Assis
+*/
 package GerandoCPF;
 
 public class Cpf {
